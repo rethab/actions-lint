21941e0b397df5af0f4449e0ea313373f5ebda04
Reto Hablützel <rethab@protonmail.ch>
2022-03-17 20:48:21 +0100

    chore(build): fail if coverage is not 100%

--- jest.config.ts
+++ jest.config.ts
@@ -8,5 +8,12 @@ module.exports = {
   },
   verbose: true,
   collectCoverage: true,
-  coverageThreshold: { global: 100 },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
